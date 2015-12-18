9c7d4fd2fc71210d506eb8c9d5bc1f23f151bdf9
Joshua Okello <joshua.okello@hotmail.com>
2015-12-18 13:16:31 +0000

    intial commit

---
+++ santa.php
@@ -0,0 +1,23 @@
+<?php
+
+//get some post data
+$callerNumber = $_POST['callerNumber'];
+$direction = $_POST['direction'];
+$sessionID = $_POST['sessionId'];
+$isCallActive = $_POST['isActive'];
+
+if ($isCallActive == 1 && $direction == "Inbound") {
+    //hang up
+    
+    //immediately call user back
+}
+
+elseif ($isCallActive == 1 && $direction == "Outbound") {
+    //redirect call to be processed on other page
+}
+
+else{
+}
+
+
+?>
